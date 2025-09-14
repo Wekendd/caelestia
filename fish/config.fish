564126947395dc9ab8570b86d5e75bf93d9c1878
Wekend <wekendmc@gmail.com>
2025-09-14 12:00:57 -0700

    rm abbr

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,8 @@ if status is-interactive
     # Custom colours
     cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
 
+    abbr -a rm 'rm -fri'
+
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
         echo -en "\e]133;A\e\\"
--- fish/functions/nano.fish
+++ fish/functions/nano.fish
@@ -1,4 +1,3 @@
 function nano --wraps=macro --wraps=micro --description 'alias nano=micro'
   micro $argv
-        
 end

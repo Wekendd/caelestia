7b5ac18526df05806f7a5e9d744b07868b5f92e2
Wekend <wekendmc@gmail.com>
2025-12-29 17:10:01 -0800

    upgrade fish and change config a bit

--- fish/config.fish
+++ fish/config.fish
@@ -9,33 +9,16 @@ if status is-interactive
     # Better ls
     alias ls='eza --icons --group-directories-first -1'
 
-    # Abbrs
-    abbr lg 'lazygit'
-    abbr gd 'git diff'
-    abbr ga 'git add .'
-    abbr gc 'git commit -am'
-    abbr gl 'git log'
-    abbr gs 'git status'
-    abbr gst 'git stash'
-    abbr gsp 'git stash pop'
-    abbr gp 'git push'
-    abbr gpl 'git pull'
-    abbr gsw 'git switch'
-    abbr gsm 'git switch main'
-    abbr gb 'git branch'
-    abbr gbd 'git branch -d'
-    abbr gco 'git checkout'
-    abbr gsh 'git show'
+    # Better cd
+    zoxide init fish | source
+    alias cd="z"
 
-    abbr l 'ls'
-    abbr ll 'ls -l'
-    abbr la 'ls -a'
-    abbr lla 'ls -la'
+    # Better rm
+    alias rm="trash"
+    # abbr -a rm 'rm -fri'
 
     # Custom colours
     cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
-
-    abbr -a rm 'rm -fri'
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
